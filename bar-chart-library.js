c035fed00cc301a99270b4245554845ac9bfd48b
Paul Ladd <vlendle@gmail.com>
2021-02-20 02:30:53 +0000

    Adding a CSS file, and removed all <canvas> references, since we're not supposed to use it

--- bar-chart-library.js
+++ bar-chart-library.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   alert("JQuery loaded properly!");
 });
+
+const drawBarChart = function (data, options, element) {
+  //Expects an array of arrays for data, and object for options, and a DOM or jQuery element for element
+
+}
