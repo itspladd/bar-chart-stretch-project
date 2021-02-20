6f461b611991c61c88ffcf3f1c2924888bc6e848
Paul Ladd <vlendle@gmail.com>
2021-02-20 21:51:49 +0000

    Added options handling to the drawBarChart function, with default values.

--- bar-chart-library.js
+++ bar-chart-library.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
   $( "button" ).click(function( event ) {
-
-    drawBarChart(1,2,$("#barChartBox"));
-    //$("#barChartBox").append("HERE'S SOME STUFF");
+    let options = {
+      width:0,
+      height:100,
+      barColour: "green"
+    }
+    drawBarChart(1,options,$("#barChartBox"));
   });
-  //alert("JQuery loaded properly!");
+  //Uncomment this line if you suspect that the entire JS file isn't being loaded.
+  //alert("bar-chart-library.js loaded!");
 });
 
 
 const drawBarChart = function (data, options, element) {
   //Expects an array of arrays for data, and object for options, and a DOM or jQuery element for element
-  //The bar charts will be built out of a stack of divs within another div.
-  //alert( "Wow! The chart is already done! That was so easy!" );
 
-  element.append(`<div style = "border: 1px black dashed">I'm a new div, inserted by drawBarChart!</div>`);
+  options["width"] = options["width"] || 100;
+  options["height"] = options["height"] || 100;
+  options["barColour"] = options["barColour"] || "fuschia";
+  options["labelColour"] = options["labelColour"] || "marmalade";
+  options["barSpacing"] = options["barSpacing"] || 10;
+  options["xLabel"] = options["xLabel"] || "X Axis";
+  options["yLabel"] = options["yLabel"] || "Y Axis";
+  options["title"] = options["title"] || "My Untitled Chart";
 
+    //The bar charts will be built out of a stack of divs within another div.
+  element.append(`<div style = "border: 1px black dashed">I'm a new div, inserted by drawBarChart! My width is ${options.width}</div>`);
 }
