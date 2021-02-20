807ef66b91aed4879e9a927a8751df80ec1e0226
Paul Ladd <vlendle@gmail.com>
2021-02-20 21:29:33 +0000

    Button now inserts a new, styled div into an existing div when pressed.

--- bar-chart-library.js
+++ bar-chart-library.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $( "button" ).click(function( event ) {
 
-    drawBarChart(1,2,3);
-
+    drawBarChart(1,2,$("#barChartBox"));
+    //$("#barChartBox").append("HERE'S SOME STUFF");
   });
   //alert("JQuery loaded properly!");
 });
@@ -11,6 +11,8 @@ $(document).ready(function() {
 const drawBarChart = function (data, options, element) {
   //Expects an array of arrays for data, and object for options, and a DOM or jQuery element for element
   //The bar charts will be built out of a stack of divs within another div.
-  alert( "Wow! The chart is already done! That was so easy!" );
+  //alert( "Wow! The chart is already done! That was so easy!" );
+
+  element.append(`<div style = "border: 1px black dashed">I'm a new div, inserted by drawBarChart!</div>`);
 
 }
