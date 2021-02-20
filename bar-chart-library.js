da4706739d7a75c09adc737ab496424165fd7b1b
Paul Ladd <vlendle@gmail.com>
2021-02-20 02:50:53 +0000

    Basic functionality of making an alert pop up when a button is clicked.

--- bar-chart-library.js
+++ bar-chart-library.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-  alert("JQuery loaded properly!");
+  $( "button" ).click(function( event ) {
+
+    drawBarChart(1,2,3);
+
+  });
+  //alert("JQuery loaded properly!");
 });
+
 
 const drawBarChart = function (data, options, element) {
   //Expects an array of arrays for data, and object for options, and a DOM or jQuery element for element
+  //The bar charts will be built out of a stack of divs within another div.
+  alert( "Wow! The chart is already done! That was so easy!" );
 
 }
