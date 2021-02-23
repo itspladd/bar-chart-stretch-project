d9d13d47be48f59edb2a4bd2629bbcc7b506dfde
Paul Ladd <vlendle@gmail.com>
2021-02-23 16:13:39 +0000

    Cleaned up documentation and added a bit more

--- bar-chart-library.js
+++ bar-chart-library.js
@@ -1,20 +1,3 @@
-/* Options are as follows:
-  width: the width of the overall chart container div, including Y-axis div. Other div dimensions are calculated based on this value.
-  height: height of the overall chart container div, including X-axis and title divs. Other div dimensions are calculated based on this value.
-  barSpacing: spacing in between each bar. Calculated based on available width after y-axis is inserted and number of bars.
-  xLabel: label of the x-axis.
-  yLabel: label of the y-axis.
-  title: title of the chart.
-  titleFont: font of chart title.
-  titleFontSize: font size of chart title. title div height is calculated from this.
-*/
-/* Data input is as follows:
-  values: an array of numerical values representing each segment of a single bar. A single value will result in a single bar.
-  labels: an array of labels that will be matched to each value in the values array.
-  barColors: an array of strings representing colors that will be matched to each value in the values array.
-  labelColors: and array of strings representing colors that will be used for each label in the labels array.
-*/
-
 $(document).ready(function() {
   let options = {
     width:99,
@@ -35,15 +18,34 @@ $(document).ready(function() {
 });
 
 
-const drawBarChart = function (data, options, element) {
-  //Expects an array of objects for data, and object for options, and a DOM or jQuery element for element
-  //Structure of data input:
-  /*data = [
+const drawBarChart = function (data, options, element, debug = false) {
+  /*Expects an array of objects for data, an object for options, and a DOM or jQuery element for element
+  ****DATA****
+  data = [
     {values: [int1, int2, ...], labels: ["label1", "label2", ...], barColors: ["color1", "color2"], labelColors: ["color1", "color2"]},
     {values: [int1, int2, ...], labels: ["label1", "label2", ...], barColors: ["color1", "color2"], labelColors: ["color1", "color2"]},
     etc
   ]
-  Each object is a single bar. The values array can have one or multiple values, which will be the number of sections in that bar.
+  values: an array of numerical values representing each segment of a single bar. A single value will result in a single bar.
+  labels: an array of labels that will be matched to each value in the values array.
+  barColors: an array of strings representing colors that will be matched to each value in the values array.
+  labelColors: and array of strings representing colors that will be used for each label in the labels array.
+
+  ****OPTIONS****
+  options = { width: number, height: number, ...}
+  A single object specifying configuration options for the chart as a whole.
+  All values are optional,
+  width: the width of the overall chart container div, including Y-axis div. Other div dimensions are calculated based on this value.
+  height: height of the overall chart container div, including X-axis and title divs. Other div dimensions are calculated based on this value.
+  barSpacing: spacing in between each bar. Calculated based on available width after y-axis is inserted and number of bars.
+  xLabel: label of the x-axis.
+  yLabel: label of the y-axis.
+  title: title of the chart.
+  titleFont: font of chart title.
+  titleFontSize: font size of chart title. title div height is calculated from this.
+
+  ****ELEMENT****
+  TODO: Add documentation here if it's warranted.
   */
 
   //Check inputs for validity. TODO: Flesh these out.
@@ -60,6 +62,7 @@ const drawBarChart = function (data, options, element) {
   const minWidth = 500;
   const minHeight = 400;
 
+  //Set any blank options to their default values.
   options["width"] = options["width"] > minWidth ? options["width"] : minWidth;
   options["height"] = options["height"] > minHeight ? options["height"] : minHeight;
   options["barSpacing"] = options["barSpacing"] || 10;
@@ -69,7 +72,7 @@ const drawBarChart = function (data, options, element) {
   options["titleFont"] = options["titleFont"] || "Comic Sans";
   options["titleFontSize"] = options["titleFontSize"] || 30;
 
-  //Style variables that aren't included in options
+  //Style variables that aren't included in the options input
   const titlePadding = 10;
   const axisFontSize = 16;
   const axisPadding = 10;
@@ -82,6 +85,7 @@ const drawBarChart = function (data, options, element) {
   const yAxisOffsetY = xAxisOffsetY + 1;
   const yAxisWidth = xAxisOffsetY - (axisPadding * 2);
 
+  //Now we're ready to start building!
   //This div is the container for all of this chart's elements.
   element.append(`<div class = "chart-container" style = "width: ${options.width}; height: ${options.height}px"></div>`);
 
